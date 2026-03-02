1f2458f45624f99f3ceb357a9910aafedc81dc0b
Dewisma-25 <madewismawardana88@gmail.com>
2026-03-02 12:40:19 +0800

    Create index.blade.php
    menambahkan index.blade.php

---
+++ barbershop/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dicta similique voluptatem illo praesentium, quam tempore error deserunt, nostrum beatae sed minus laborum inventore perspiciatis. At ea et corporis quam?
+    </p>
+</body>
+</html>
