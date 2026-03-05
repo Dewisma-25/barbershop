b88ced3c251ffbd0bb758aa641e7fac4460aec9b
Dewisma-25 <madewismawardana88@gmail.com>
2026-03-05 09:12:31 +0800

    Update index.blade.php
    menambahkan h1

--- barbershop/resources/views/index.blade.php
+++ barbershop/resources/views/index.blade.php
@@ -9,5 +9,9 @@
     <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dicta similique voluptatem illo praesentium, quam tempore error deserunt, nostrum beatae sed minus laborum inventore perspiciatis. At ea et corporis quam?
     </p>
+
+    <h1>
+        INi adalahan update pada kamis 5 Maret 2026
+    </h1>
 </body>
 </html>
